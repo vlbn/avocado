578f9850011b6cf1835d8ecd51720b0c04c51183
vlbn <valbuena@duck.com>
2024-01-04 21:58:26 +0100

    tiempos ajustado

--- scripts/zzz.sh
+++ scripts/zzz.sh
@@ -1,5 +1,5 @@
-swayidle -w timeout 10 'swaylock -f -c 000000' \
-            timeout 15 'hyprctl dispatch dpms off' \
+swayidle -w timeout 60 'swaylock -f -c 000000' \
+            timeout 90 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            timeout 20 'systemctl suspend' \
+            timeout 120 'systemctl suspend' \
             before-sleep 'swaylock -f -c 000000' &
