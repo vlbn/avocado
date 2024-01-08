952f34039ae567904b1b05f0b4851f4515ecfa66
vlbn <valbuena@duck.com>
2024-01-08 16:05:48 +0100

    gg Zzz

--- scripts/zzz.sh
+++ scripts/zzz.sh
@@ -1,5 +1,6 @@
-swayidle -w timeout 60 'swaylock -f -c 000000' \
-            timeout 90 'hyprctl dispatch dpms off' \
+#!/bin/bash
+swayidle -w timeout 30 'swaylock -f -c 000000' \
+            timeout 60 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            timeout 120 'systemctl suspend' \
+            timeout 300 'systemctl suspend' \
             before-sleep 'swaylock -f -c 000000' &
