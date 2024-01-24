de0533c6c84ac9fcf9df80a462434210544375e2
vlbn <valbuena@duck.com>
2024-01-24 15:55:07 +0100

    ren

--- scripts/networkmanager.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-iwctl adapter phy0 set-property Powered on
-read -p "..." -t .1
-pkill nmtui || nmtui
--- scripts/zzz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-swayidle -w timeout 30 'swaylock -f -c 000000' \
-            timeout 60 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 300 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
